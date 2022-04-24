c034a7c218c09242745950302981206dd6b13f85
Brian Slesinsky <brian-git@slesinsky.org>
2022-04-23 17:35:25 -0700

    added pause button, report errors

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
 import { Terminal } from 'xterm';
 export {}
 
-let connectElt = document.getElementById("connect") as HTMLButtonElement;
-let terminalElt = document.getElementById("terminal") as HTMLDivElement;
+var [startElt, connectedElt, terminalElt] =
+  ["start", "connected", "terminal"].map((id) => document.getElementById(id)) as HTMLDivElement[];
+
+let [connectElt, pauseElt] =
+  ["connect", "pause"].map((id) => document.getElementById(id)) as HTMLButtonElement[];
 
 let terminal = new Terminal({
   rows: 50,
@@ -10,39 +13,73 @@ let terminal = new Terminal({
 });
 terminal.open(terminalElt);
 
-var connected = false;
-
 async function connect() {
-    
     connectElt.disabled = true;
 
     let port = await navigator.serial.requestPort();
 
-    connected = true;
     let info = port.getInfo();
     console.log(`Connecting to ${info.usbVendorId} ${info.usbProductId}`);
 
     await port.open({
       baudRate: 115200,
-      bufferSize: 4096,
+      bufferSize: 40,
       flowControl: "hardware",
      });
-    connectElt.textContent = "Connected";
 
-    terminal.clear();
+    onConnected(port);
+}
 
+function onConnected(port: SerialPort) {
+
+  var paused = false;
+
+  var copyDone: Promise<void>;
+
+  function writeStatus(e) {
+    terminal.write(`\n*** ${e} ***\r\n\r\n`);
+  }
+
+  function fatal(e) {
+    writeStatus(e);
+    pauseElt.disabled = true;
+  }
+
+  async function copyToTerminal(): Promise<void> {
     let reader = port.readable.getReader();
     try {
       while (true) {
         const { value, done } = await reader.read();
-        if (done) {
-          break;
-        }  
         terminal.write(value);
+        if (paused) {
+          writeStatus("Paused");
+          return;
+        } else if (done) {
+          fatal("Done");
+          return;
+        }
       }
+    } catch(e) {
+      fatal(e);
     } finally {
       reader.releaseLock();
     }
- }
+  }
+
+  function onPause() {
+    paused = !paused;
+    pauseElt.textContent = paused ? "Resume" : "Pause";
+    if (!paused) {
+      copyDone.then(() => {
+        copyDone = copyToTerminal();
+      });
+    }
+  }
+
+  startElt.style.display = "none";
+  connectedElt.style.display = "block";
+  copyDone = copyToTerminal();
+  pauseElt.addEventListener("click", onPause);
+}
 
 connectElt.addEventListener("click", connect);
