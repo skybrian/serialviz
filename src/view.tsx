e278a565563a63c215a24f5fe9f954057bbfad25
Brian Slesinsky <brian-git@slesinsky.org>
2022-05-17 20:19:38 -0700

    move the terminal view to its own file

--- src/view.tsx
+++ src/view.tsx
@@ -1,10 +1,9 @@
 'use strict';
 
-import { h, Component, ComponentChildren, toChildArray, createRef, VNode } from 'preact';
-import { Terminal } from 'xterm';
-import { FitAddon } from 'xterm-addon-fit';
-import { LogLine, AppProps, SelectedTab } from './state';
+import { h, ComponentChildren, toChildArray, VNode } from 'preact';
+import { AppProps, SelectedTab } from './state';
 import { PlotView } from './plot';
+import { TermView } from './term';
 
 interface ConnectProps {
   haveSerial: boolean;
@@ -112,69 +111,4 @@ const TabView = (props: TabProps) => {
       }
     })}
   </div>
-}
-
-interface TermProps {
-  logKey: number;
-  lines: LogLine[];
-  truncateRows?: boolean;
-  windowChanges: number;
-}
-
-class TermView extends Component<TermProps> {
-  terminal: Terminal;
-  fitAddon: FitAddon;
-
-  currentLog = 0;
-  lastKeyWritten = -1;
-  linesWritten = 0;
-  lastWindowChangeSeen: number;
-
-  terminalElt = createRef();
-
-  componentDidMount() {
-    this.terminal = new Terminal({
-      scrollback: 0,
-    });
-    this.fitAddon = new FitAddon();
-    this.terminal.loadAddon(this.fitAddon);
-    this.terminal.open(this.terminalElt.current);
-    this.fitAddon.fit();
-    this.componentDidUpdate();
-  }
-
-  componentDidUpdate() {
-    const newDim = this.fitAddon.proposeDimensions();
-
-    // Clear terminal if needed
-    if (this.currentLog != this.props.logKey ||
-      (this.props.truncateRows && newDim.rows != this.terminal.rows)) {
-      this.terminal.clear();
-      this.currentLog = this.props.logKey;
-      this.lastKeyWritten = -1;
-      this.linesWritten = 0;
-    }
-
-    // Resize terminal if needed
-    if (this.props.windowChanges != this.lastWindowChangeSeen) {
-      this.fitAddon.fit();
-      this.lastWindowChangeSeen = this.props.windowChanges;
-    }
-
-    // Append lines if needed
-    for (let line of this.props.lines) {
-      if (this.props.truncateRows && this.linesWritten >= this.terminal.rows - 1) {
-        break;
-      }
-      if (line.key > this.lastKeyWritten) {
-        this.terminal.writeln(line.value);
-        this.lastKeyWritten = line.key;
-        this.linesWritten++;
-      }
-    }
-  }
-
-  render() {
-    return <div class="term-view" ref={this.terminalElt}></div>
-  }
 }
